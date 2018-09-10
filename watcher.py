a04c6f41aaaabc4894c4b7ec6006af8f1e5ef58d
Takuro Fukamizu <takuro.f.1987@gmail.com>
2018-09-10 16:54:03 +0900

    python test code is working

---
+++ watcher.py
@@ -0,0 +1,29 @@
+import subprocess
+import csv
+from collections import namedtuple
+
+cmd = ("nvidia-smi --query-gpu=name,index,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv,noheader,nounits")
+
+GpuRecord = namedtuple('GpuRecord', ('name', 'index', 'gpu_temperature', 'gpu_utilization', 'memory_utilization', 'memory_total', 'memory_free', 'memory_used'))
+
+def res_cmd(cmd):
+    return subprocess.Popen(cmd, stdout=subprocess.PIPE,shell=True).stdout.readlines()
+
+def parse_res(res):
+    """
+    res: csv test array
+    """
+    reader = csv.reader(res)
+    for row in reader:
+        record = GpuRecord(*row)
+        yield record
+
+if __name__=='__main__':
+    data = ''
+    text_list = res_cmd(cmd)
+    for record in parse_res([ text.decode('utf-8') for text in text_list ]): 
+        print(record)
+    #for text in records:
+    #    text = text.decode('utf-8')
+    #    data += text + "\n"
+
